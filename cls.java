8f3e8e4f81f23fa7eea788d2b6290f9e64913e2b
Harishakthivelp <pharishakthivel@gmail.com>
2021-12-07 12:19:40 +0530

    java class program

---
+++ cls.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class Student:
+{
+String name = "Hari";
+int Age = 22;
+public void display(){
+System.out.println("This is"+name+"age is "+Age)
+}
+}
+public static void main(String args[])
+{
+Students s = new Students();
+s.display();
+
+}
